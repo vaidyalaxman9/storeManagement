6760f2a971d3bc481158d1606372b2a03194f208
Laxman <vaidyalaxman9@gmail.com>
2023-01-05 22:10:09 +0530

    liquibase

---
+++ src/test/java/com/home/practice/storeManagement/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.practice.storeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
